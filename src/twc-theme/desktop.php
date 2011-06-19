1f256712e07693a4dfbeda67692cac5587774ce6
Patryk Rzucidlo <ptkdev@gmail.com>
2011-06-19 11:20:49 +0200

    Test1: Desktop Layout Alpha 1

--- src/twc-theme/desktop.php
+++ src/twc-theme/desktop.php
@@ -26,8 +26,5 @@
  */
 ?>
 
-	<div id="content">	
-		<img src="<?php bloginfo('template_url'); ?>/img/desktop/twc.png" alt="TwitCrusader" title="TwitCrusader" /><br />
-		<div class="clear"></div>
 	</div>
 </div>
